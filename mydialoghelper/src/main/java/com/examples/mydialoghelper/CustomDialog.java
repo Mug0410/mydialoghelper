c3ddbe88aa24a5b50298dd4b6f750da4927121d2
Mug0410 <mugdho.c@paythrough.in>
2021-04-28 13:53:07 +0530

    muggdho1

--- mydialoghelper/src/main/java/com/examples/mydialoghelper/CustomDialog.java
+++ mydialoghelper/src/main/java/com/examples/mydialoghelper/CustomDialog.java
@@ -4,8 +4,10 @@ package com.examples.mydialoghelper;
 import android.content.Context;
 import android.content.DialogInterface;
 
+import android.graphics.Color;
 import android.view.View;
 
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.Toast;
@@ -21,21 +23,39 @@ public class CustomDialog {
         builder.setTitle(title);
         builder.setMessage(mesage);
         builder.setPositiveButton(pBtnTitle,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(context,
-                                pButnClickMsg,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-        builder.setNegativeButton(nBtnTitle, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-               dialog.dismiss();
-            }
-        });
+                (dialog, which) -> Toast.makeText(context,
+                        pButnClickMsg,
+                        Toast.LENGTH_SHORT).show());
+        builder.setNegativeButton(nBtnTitle, (dialog, which) -> dialog.dismiss());
         builder.setCancelable(false);
         builder.create().show();
     }
+
+
+
+    public void withItems(Context context, String[] items,String title,String pBtnTitle,String negBtnTitle,String neuBtnTitle,String dialogMes) {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(title)
+
+                .setItems(items, (dialog, which) -> Toast.makeText(context, dialogMes, Toast.LENGTH_SHORT).show());
+
+        builder.setPositiveButton(pBtnTitle, null);
+        builder.setNegativeButton(negBtnTitle, null);
+        builder.setNeutralButton(neuBtnTitle, null);
+
+
+        AlertDialog alertDialog = builder.create();
+
+        alertDialog.show();
+
+        Button button = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        button.setBackgroundColor(Color.BLACK);
+        button.setPadding(0, 0, 20, 0);
+        button.setTextColor(Color.WHITE);
+    }
+
 
 
 
